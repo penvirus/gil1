cf2ac3eb8abfab866ce8c67eb35801249faadcef
Tzung-Bi Shih <penvirus@gmail.com>
2015-05-12 17:47:01 +0800

    compare mp, mt, and st

---
+++ example1.py
@@ -0,0 +1,40 @@
+import time
+from multiprocessing import Process
+from threading import Thread
+from multiprocessing import Queue as MPQ
+from Queue import Queue
+
+MAX = 1000000
+
+def test_(w_class, q_class):
+    def worker(queue):
+        for i in xrange(MAX):
+            queue.put(i)
+
+    q = q_class()
+    w = w_class(target=worker, args=(q,))
+
+    begin = time.time()
+    w.start()
+    for i in xrange(MAX):
+        q.get()
+    w.join()
+    end = time.time()
+
+    return end - begin
+
+def test_sthread():
+    q = Queue()
+
+    begin = time.time()
+    for i in xrange(MAX):
+        q.put(i)
+        q.get()
+    end = time.time()
+
+    return end - begin
+
+if __name__ == '__main__':
+    print 'mprocess: %.6f' % test_(Process, MPQ)
+    print 'mthread:  %.6f' % test_(Thread, Queue)
+    print 'sthread:  %.6f' % test_sthread()
